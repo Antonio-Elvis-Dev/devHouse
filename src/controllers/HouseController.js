2826e864cc2f693943e8b7e9653907827ee0135b
Antônio Elvis <102705791+Antonio-Elvis-Dev@users.noreply.github.com>
2023-03-24 14:35:22 -0300

    get, put, post e delete concluidos

--- src/controllers/HouseController.js
+++ src/controllers/HouseController.js
@@ -1,8 +1,6 @@
 import House from "../models/House";
-import House from "../models/House";
 import User from "../models/User";
 class HouseController {
-
   async index(req, res) {
     const { status } = req.query;
     const houses = await House.find({ status });
@@ -25,6 +23,7 @@ class HouseController {
 
     return res.json(house);
   }
+
   async update(req, res) {
     const { filename } = req.file;
     const { house_id } = req.params;
@@ -32,10 +31,11 @@ class HouseController {
     const { user_id } = req.headers;
 
     const user = await User.findById(user_id);
+
     const houses = await House.findById(house_id);
 
     if (String(user._id) !== String(houses.user)) {
-      return res.status(401).json({error:'Não Autorizado'})
+      return res.status(401).json({ error: "Não Autorizado" });
     }
 
     await House.updateOne(
@@ -52,15 +52,22 @@ class HouseController {
 
     return res.send();
   }
-  // async destroy(req,res){
-  //   const {house_id} = req.body
-  //   const {user_id} = req.readers
 
-  //   await House.findByIdAndDelete({_id:house_id})
+  async destroy(req, res) {
+    const { house_id } = req.body;
+    const { user_id } = req.headers;
 
-  //   return res.json({message:'Casa Excluída!'})
+    const user = await User.findById(user_id);
+    const houses = await House.findById(house_id);
 
-  // }
+    if (String(user._id) !== String(houses.user)) {
+      return res.status(401).json({ error: "Não Autorizado" });
+    }
+
+    await House.findByIdAndDelete({_id:house_id})
+
+    return res.json({ message: "Casa Excluida" });
+  }
 }
 
 export default new HouseController();
